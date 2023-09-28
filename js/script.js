4d34c11de4dc7757d7fc94e684c5b791c9fdf6b4
frakibuddin <frakibuddin360@gmail.com>
2023-09-29 00:36:39 +0600

    upload porject

---
+++ js/script.js
@@ -0,0 +1,57 @@
+$(document).ready(function () {
+  $(".counter-value").each(function () {
+    $(this)
+      .prop("Counter", 0)
+      .animate(
+        {
+          Counter: $(this).text(),
+        },
+        {
+          duration: 3500,
+          easing: "swing",
+          step: function (now) {
+            $(this).text(Math.ceil(now));
+          },
+        }
+      );
+  });
+  // testimonail
+  var owl_testimonail = $("#owl-testimonail");
+
+  owl_testimonail.owlCarousel({
+    items: 1, //10 items above 1000px browser width
+    itemsDesktop: [1000, 1], //5 items between 1000px and 901px
+    itemsDesktopSmall: [900, 1], // betweem 900px and 601px
+    itemsTablet: [600, 1], //2 items between 600 and 0
+    itemsMobile: false, // itemsMobile disabled - inherit from itemsTablet option
+    pagination: false,
+  });
+
+  // Custom Navigation Events
+  $(".next").click(function () {
+    owl_testimonail.trigger("owl.next");
+  });
+  $(".prev").click(function () {
+    owl_testimonail.trigger("owl.prev");
+  });
+
+  // Brand slider
+  var owl_brand = $("#owl-brand");
+
+  owl_brand.owlCarousel({
+    items: 5, //10 items above 1000px browser width
+    itemsDesktop: [1000, 4], //5 items between 1000px and 901px
+    itemsDesktopSmall: [900, 3], // betweem 900px and 601px
+    itemsTablet: [600, 2], //2 items between 600 and 0
+    itemsMobile: false, // itemsMobile disabled - inherit from itemsTablet option
+    pagination: false,
+  });
+
+  // Custom Navigation Events
+  $(".next").click(function () {
+    owl_brand.trigger("owl.next");
+  });
+  $(".prev").click(function () {
+    owl_brand.trigger("owl.prev");
+  });
+});
